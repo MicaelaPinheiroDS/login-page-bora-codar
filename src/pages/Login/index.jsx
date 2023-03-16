77fd547f5fdcba741e96f58c5bdc77c35dd9e920
Micaela Pinheiro <micaela.mews@gmail.com>
2023-03-16 17:14:16 -0300

    create login page

---
+++ src/pages/Login/index.jsx
@@ -0,0 +1,17 @@
+export function Login(){
+    return(
+        <div className="flex justify-between">
+            <div className="md:mx-28 mx-auto">
+            <header className="pt-10">
+                <h1><img src="./public/images/logo.svg"  alt="logo da vertigo"/></h1>
+            </header>
+            <main>
+
+            </main>
+            </div>
+            <div className="md:bg-[url(./public/images/img.jpg)] md:bg-cover md:bg-bottom md:w-[50%] md:h-screen">
+
+            </div>
+        </div>
+    )
+}
